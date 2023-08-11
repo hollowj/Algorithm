a1c72b47552f1aad1c319ce9637ac39efe3ab951
胡文俊(Hu WenJun/弥勒法师) <hu.wenjun1@olaola.chat>
2023-08-11 14:44:12 +0800

    更好理解的背包二维dp

--- dp/t494_test.go
+++ dp/t494_test.go
@@ -40,21 +40,27 @@ func findTargetSumWays(nums []int, target int) int {
 		return 0
 	}
 	result := (target + sum) / 2
-	dp := make([][]int, len(nums)+1)
-	for i := 0; i <= len(nums); i++ {
+	dp := make([][]int, len(nums))
+	for i := 0; i < len(nums); i++ {
 		dp[i] = make([]int, result+1)
 	}
 	dp[0][0] = 1
-	for i := 1; i <= len(nums); i++ {
+	if result >= nums[0] {
+		dp[0][nums[0]] += 1
+	}
+
+	for i := 1; i < len(nums); i++ {
 		for j := 0; j <= result; j++ {
-			dp[i][j] = dp[i-1][j]
-			if j >= nums[i-1] {
-				dp[i][j] += dp[i-1][j-nums[i-1]]
+			if j >= nums[i] {
+				dp[i][j] = dp[i-1][j-nums[i]] + dp[i-1][j]
+			} else {
+				dp[i][j] = dp[i-1][j]
 			}
 		}
 	}
-	return dp[len(nums)][result]
+	return dp[len(nums)-1][result]
 }
+
 func TestT494(t *testing.T) {
 	assert.Equal(t, 5, findTargetSumWays([]int{1, 1, 1, 1, 1}, 3))
 	assert.Equal(t, 1, findTargetSumWays([]int{1}, 1))
